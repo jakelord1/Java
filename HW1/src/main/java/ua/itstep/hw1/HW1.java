fbc0cd4ca8988c4b64a5cfee4f99372cd0eafcf3
jakelord1 <jakelord1.vo@gmail.com>
2025-09-02 10:09:05 +0300

    Project add

---
+++ HW1/src/main/java/ua/itstep/hw1/HW1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ua.itstep.hw1;
+
+/**
+ *
+ * @author pronc
+ */
+public class HW1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
